7b995dad28cb7b2eda0c958ceaa0f6088a8b0681
Steven Kirk <grokys@gmail.com>
2020-03-26 12:46:23 +0100

    Initial implementation of avaloniaui.net.
    Index and support page.

---
+++ src/AvaloniaUI.Homepage/wwwroot/js/navbar.js
@@ -0,0 +1,27 @@
+$(function() {
+  'use strict';
+
+  var navbar = $('#navbar');
+
+  // Sticky Menu
+  $(window).scroll(function () {
+    if (navbar.offset().top > 10) {
+      navbar.addClass('bg');
+    } else {
+      navbar.removeClass('bg');
+    }
+  });
+
+  // Hamburger menu
+  $('#toplevel-menu-toggle').click(function() {
+    navbar.toggleClass('open');
+  });
+
+  $('body').click(function() {
+    navbar.removeClass('open');
+  });
+
+  navbar.click(function (e) {
+    e.stopPropagation();
+  });
+})
